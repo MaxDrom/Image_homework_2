1e481c562526d6f4895e64317e9ffb19837c9447
MaxDrom <maxidrom310@gmail.com>
2023-03-20 07:21:24 +0300

    initial

---
+++ 1/prog.py
@@ -0,0 +1,19 @@
+import numpy as np
+from scipy.fftpack import fft2, ifft2
+import matplotlib.pyplot as plt
+n = 100
+image = np.random.random((n, n))
+fft_image = fft2(image) 
+sorted_ind =np.argsort( np.real(fft_image*np.conjugate(fft_image)), axis=None)
+x = []
+y = []
+for k in range(n):
+    x.append(k/n*100)
+    fft_image[np.unravel_index(sorted_ind[:k], fft_image.shape)] = 0
+    new_image = np.real(ifft2(fft_image))
+    y .append(np.mean(1-np.abs(image-new_image)/image))
+
+plt.plot(x,y)
+plt.xlabel("%")
+plt.ylabel("Quality")
+plt.show()
---
+++ 2/prog.py
@@ -0,0 +1,51 @@
+from astropy.io import fits
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.fftpack import fft2, fftshift, ifft2
+from scipy.ndimage import convolve
+
+def do_plot(name, data):
+    fig, ax = plt.subplots()
+    ax.imshow(1000*np.abs(data))
+    fig.set_figwidth(data.shape[0]/100)   
+    fig.set_figheight(data.shape[1]/100)    
+    #plt.show()
+    plt.savefig(name)
+
+def mean_nearest(x, y, data):
+    result = 0
+    for i in range(x-1, x+2):
+        for j in range(y-1, y+2):
+            if (i-x)**2+(j-y)**2 != 0:
+                result+= data[i, j]
+    return result
+
+with fits.open("noised.fits") as hdu_list:
+    data = hdu_list[0].data
+
+(X, Y) = data.shape
+
+fourier_img = fft2(data) 
+fourier_shifted = fftshift(fourier_img)
+do_plot("fft.png",np.abs(fourier_shifted))
+
+kernel = 1/8*np.ones((3,3))
+kernel[1,1] = 0
+four_mean = convolve(fourier_img, kernel)
+difference = fourier_img - four_mean
+four_mean_sd = convolve(difference*np.conjugate(difference),kernel)
+four_mean_sd = np.sqrt(np.real(four_mean_sd))
+for i in range(X):
+    for j in range(Y):
+        if abs(fourier_img[i, j] - four_mean[i,j])> 5*four_mean_sd[i,j]:
+            fourier_img[i,j] = four_mean[i,j]
+
+fourier_shifted = fftshift(fourier_img)
+do_plot("fft_res.png",np.abs(fourier_shifted))
+
+new_data = ifft2(fourier_img)
+
+
+hdu = fits.PrimaryHDU(data=np.real(new_data))
+hdu_list = fits.HDUList([hdu])
+hdu_list.writeto('result.fits', overwrite=True)
